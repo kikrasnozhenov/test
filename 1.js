2da2f22f3ee1fb9a67814da35e775d67da104bfb
Kirill Krasnozhenov <krasnozhenovk@gmail.com>
2017-05-16 21:24:09 +0300

    test with functions

--- 1.js
+++ 1.js
@@ -11,3 +11,18 @@ var form = document.querySelector('form');
 form.addEventListener("click", function(event){
     event.target.style.background = "#fff";
 });
+
+const square = function(a){
+  return a * a;
+}
+console.log(square(10));
+
+const sumOfSquares = function(a, b){
+  return square(a) + square(b);
+}
+console.log(sumOfSquares(10, -9));
+
+const squareSumOfSquares = function(a, b){
+  return square(sumOfSquares(a, b));
+}
+console.log(squareSumOfSquares(1, 1));
